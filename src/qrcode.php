9c87492c3c1c73066fa4664d912d25b5e7ce211f
Kim chen <kimchen@jourdeness.com.tw>
2016-11-05 05:07:29 +0800

    first commit

---
+++ src/qrcode.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace killworm737\qrcode;
+
+class Qrcode
+{
+    private $spec_key = "Dt8lyToo17X/XkXaQvihuA==";
+    public function __construct()
+    {
+
+    }
+
+    public function aes128_cbc_encrypt($aesKey, $invoice_random)
+    {
+        $iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
+        $key = hex2bin($aesKey);
+        $iv = base64_decode($this->spec_key);
+        $data = pkcs5_pad($invoice_random,$iv_size);
+
+        return base64_encode(
+                    mcrypt_encrypt(
+                        MCRYPT_RIJNDAEL_128,
+                        $key,
+                        $data,
+                        MCRYPT_MODE_CBC,
+                        $iv
+                    )
+                );
+    }
+
+    private function pkcs5_pad ($text, $blocksize)
+    {
+        $pad = $blocksize - (strlen($text) % $blocksize);
+        return $text . str_repeat(chr($pad), $pad);
+    }
+
+
+}
